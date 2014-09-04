448b17a7d21210cd3a2e76865bd972448f87dd1f
sesteva <santiagoesteva@gmail.com>
2014-09-04 14:08:25 -0500

    add global config and alias

--- install/install-git.sh
+++ install/install-git.sh
@@ -5,3 +5,9 @@ sudo apt-get install -y git-core
 git config --global user.name $GIT_USER
 git config --global user.email $GIT_EMAIL
 git config --global core.autocrlf input
+git config --global pull.rebase true
+git congig --global color.ui true
+git config --global rerere.enabled true
+git config --global alias.s "status -s"
+git config --global alias.lg "log --oneline --decorate --all --graph"
+
