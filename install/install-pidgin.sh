b0049d5219218fb2061f654dcb0168285dfe0e28
Santiago Esteva <santiagoesteva@gmail.com>
2015-01-09 14:34:39 -0600

    updated pidgin script

--- install/install-pidgin.sh
+++ install/install-pidgin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Installing Pidgin"
-sudo apt-get install -y pidgin
+sudo apt-get install -y pidgin pidgin-sipe
 sudo sed -i '1s/^/export NSS_SSL_CBC_RANDOM_IV=0\n/' /etc/envrionment
